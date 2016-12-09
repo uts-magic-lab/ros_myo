7a152857693d7769c84a593a38eb0e454d52cbb6
Sam Pfeiffer <sam.pfeiffer@pal-robotics.com>
2016-12-09 22:53:24 +1100

    Cleaning up even more

--- scripts/myo-rawNode.py
+++ scripts/myo-rawNode.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 import enum
 import re
-import struct
 import sys
 import threading
 import time
@@ -13,10 +12,11 @@ import serial
 from serial.tools.list_ports import comports
 from common import *
 import rospy
-from std_msgs.msg import String, UInt8, Header, MultiArrayLayout, MultiArrayDimension, Float64MultiArray
+from std_msgs.msg import String, UInt8, Header
 from geometry_msgs.msg import Quaternion, Vector3
 from sensor_msgs.msg import Imu
-from ros_myo.msg import MyoArm, EmgArray
+from ros_myo.msg import MyoArm, MyoPose, EmgArray
+
 
 def multichr(ords):
     if sys.version_info[0] >= 3:
@@ -24,30 +24,12 @@ def multichr(ords):
     else:
         return ''.join(map(chr, ords))
 
+
 def multiord(b):
     if sys.version_info[0] >= 3:
         return list(b)
     else:
         return map(ord, b)
-
-class Arm(enum.Enum):
-    UNKNOWN = 0
-    RIGHT = 1
-    LEFT = 2
-
-class XDirection(enum.Enum):
-    UNKNOWN = 0
-    X_TOWARD_WRIST = 1
-    X_TOWARD_ELBOW = 2
-
-class Pose(enum.Enum):
-    REST = 0
-    FIST = 1
-    WAVE_IN = 2
-    WAVE_OUT = 3
-    FINGERS_SPREAD = 4
-    THUMB_TO_PINKY = 5
-    UNKNOWN = 255
 
 
 class Packet(object):
@@ -71,7 +53,7 @@ class BT(object):
         self.lock = threading.Lock()
         self.handlers = []
 
-    ## internal data-handling methods
+    # internal data-handling methods
     def recv_packet(self, timeout=None):
         t0 = time.time()
         self.ser.timeout = None
@@ -123,23 +105,28 @@ class BT(object):
         self.handlers.append(h)
 
     def remove_handler(self, h):
-        try: self.handlers.remove(h)
-        except ValueError: pass
+        try:
+            self.handlers.remove(h)
+        except ValueError:
+            pass
 
     def wait_event(self, cls, cmd):
         res = [None]
+
         def h(p):
             if p.cls == cls and p.cmd == cmd:
                 res[0] = p
+
         self.add_handler(h)
         while res[0] is None:
             self.recv_packet()
         self.remove_handler(h)
         return res[0]
 
-    ## specific BLE commands
+    # specific BLE commands
     def connect(self, addr):
-        return self.send_command(6, 3, pack('6sBHHHH', multichr(addr), 0, 6, 6, 64, 0))
+        return self.send_command(6, 3, pack('6sBHHHH', multichr(addr),
+                                 0, 6, 6, 64, 0))
 
     def get_connections(self):
         return self.send_command(0, 6)
@@ -168,11 +155,11 @@ class BT(object):
         while True:
             p = self.recv_packet()
 
-            ## no timeout, so p won't be None
+            # no timeout, so p won't be None
             if p.typ == 0:
                 return p
 
-            ## not a response: must be an event
+            # not a response: must be an event
             self.handle_event(p)
 
 
@@ -204,13 +191,13 @@ class MyoRaw(object):
         self.bt.recv_packet(timeout)
 
     def connect(self):
-        ## stop everything from before
+        # stop everything from before
         self.bt.end_scan()
         self.bt.disconnect(0)
         self.bt.disconnect(1)
         self.bt.disconnect(2)
 
-        ## start scanning
+        # start scanning
         print('scanning...')
         self.bt.discover()
         while True:
@@ -222,12 +209,12 @@ class MyoRaw(object):
                 break
         self.bt.end_scan()
 
-        ## connect and wait for status event
+        # connect and wait for status event
         conn_pkt = self.bt.connect(addr)
         self.conn = multiord(conn_pkt.payload)[-1]
         self.bt.wait_event(3, 0)
 
-        ## get firmware version
+        # get firmware version
         fw = self.read_attr(0x17)
         _, _, _, _, v0, v1, v2, v3 = unpack('BHBBHHHH', fw.payload)
         print('firmware version: %d.%d.%d.%d' % (v0, v1, v2, v3))
@@ -235,58 +222,60 @@ class MyoRaw(object):
         self.old = (v0 == 0)
 
         if self.old:
-            ## don't know what these do; Myo Connect sends them, though we get data
-            ## fine without them
+            # don't know what these do
+            # Myo Connect sends them, though we get data
+            # fine without them
             self.write_attr(0x19, b'\x01\x02\x00\x00')
             self.write_attr(0x2f, b'\x01\x00')
             self.write_attr(0x2c, b'\x01\x00')
             self.write_attr(0x32, b'\x01\x00')
             self.write_attr(0x35, b'\x01\x00')
 
-            ## enable EMG data
+            # enable EMG data
             self.write_attr(0x28, b'\x01\x00')
-            ## enable IMU data
+            # enable IMU data
             self.write_attr(0x1d, b'\x01\x00')
 
-            ## Sampling rate of the underlying EMG sensor, capped to 1000. If it's
-            ## less than 1000, emg_hz is correct. If it is greater, the actual
-            ## framerate starts dropping inversely. Also, if this is much less than
-            ## 1000, EMG data becomes slower to respond to changes. In conclusion,
-            ## 1000 is probably a good value.
+            # Sampling rate of the underlying EMG sensor, capped to 1000.
+            # If it's less than 1000, emg_hz is correct. If it is greater,
+            # the actual framerate starts dropping inversely. Also, if this
+            # is much less than 1000, EMG data becomes slower to respond to
+            # changes. In conclusion, 1000 is probably a good value.
             C = 1000
             emg_hz = 50
-            ## strength of low-pass filtering of EMG data
+            # strength of low-pass filtering of EMG data
             emg_smooth = 100
 
             imu_hz = 50
 
-            ## send sensor parameters, or we don't get any data
-            self.write_attr(0x19, pack('BBBBHBBBBB', 2, 9, 2, 1, C, emg_smooth, C // emg_hz, imu_hz, 0, 0))
+            # send sensor parameters, or we don't get any data
+            self.write_attr(0x19, pack('BBBBHBBBBB', 2, 9, 2, 1, C,
+                                       emg_smooth, C // emg_hz, imu_hz, 0, 0))
 
         else:
             name = self.read_attr(0x03)
             print('device name: %s' % name.payload)
 
-            ## enable IMU data
+            # enable IMU data
             self.write_attr(0x1d, b'\x01\x00')
-            ## enable on/off arm notifications
+            # enable on/off arm notifications
             self.write_attr(0x24, b'\x02\x00')
 
-            # self.write_attr(0x19, b'\x01\x03\x00\x01\x01')
             self.start_raw()
 
-        ## add data handlers
+        # add data handlers
         def handle_data(p):
-            if (p.cls, p.cmd) != (4, 5): return
+            if (p.cls, p.cmd) != (4, 5):
+                return
 
             c, attr, typ = unpack('BHB', p.payload[:4])
             pay = p.payload[5:]
 
             if attr == 0x27:
                 vals = unpack('8HB', pay)
-                ## not entirely sure what the last byte is, but it's a bitmask that
-                ## seems to indicate which sensors think they're being moved around or
-                ## something
+                # not entirely sure what the last byte is,
+                # but it's a bitmask that seems to indicate which
+                # sensors think they're being moved around or something
                 emg = vals[:8]
                 moving = vals[8]
                 self.on_emg(emg, moving)
@@ -299,40 +288,27 @@ class MyoRaw(object):
             elif attr == 0x23:
                 if len(pay) == 6:
                     try:
-                        typ, val, xdir, _,_,_ = unpack('6B', pay)
+                        typ, val, xdir, _, _, _ = unpack('6B', pay)
                     except Exception as e:
                         print("Got exception: " + str(e) + "\nContinuing...")
                         return
                 elif len(pay) == 3:
                     try:
                         typ, val, xdir = unpack('3B', pay)
-                        val = str(val)
-                        xdir = str(xdir)
-                        print("Got gesture... typ, val" + str(typ) + " " + str(val))
-                        print("type typ, val: " +  str(type(typ)) + " " + str(type(val)))
                     except Exception as e:
                         print("Got exception: " + str(e) + "\nContinuing...")
                         return
 
-                if typ == 1: # on arm
-                    print("On arm...")
-                    print("val: " + str(val))
-                    print("xdir: " + str(xdir))
-                    arm_thing = Arm(val)
-                    print("arm_thing: " + str(type(arm_thing)) + " " + str(arm_thing))
-                    #import pdb; pdb.set_trace()
-                    self.on_arm(Arm(val), XDirection(xdir))
-                elif typ == 2: # removed from arm
-                    print("On arm... type 2 bullshit")
-                    self.on_arm(Arm('0'), XDirection('0'))
-                elif typ == 3: # pose
-                    print("Its a pose!!")
-                    self.on_pose(Pose(val))
+                if typ == 1:  # on arm
+                    self.on_arm(MyoArm(arm=val, xdir=xdir))
+                elif typ == 2:  # removed from arm
+                    self.on_arm(MyoArm(MyoArm.UNKNOWN, MyoArm.UNKNOWN))
+                elif typ == 3:  # pose
+                    self.on_pose(MyoPose(val+1))
             else:
                 print('data with unknown attr: %02X %s' % (attr, p))
 
         self.bt.add_handler(handle_data)
-
 
     def write_attr(self, attr, val):
         if self.conn is not None:
@@ -353,14 +329,12 @@ class MyoRaw(object):
         '''
 
         self.write_attr(0x28, b'\x01\x00')
-        #self.write_attr(0x19, b'\x01\x03\x01\x01\x00')
         self.write_attr(0x19, b'\x01\x03\x01\x01\x01')
 
     def mc_start_collection(self):
         '''Myo Connect sends this sequence (or a reordering) when starting data
-        collection for v1.0 firmware; this enables raw data but disables arm and
-        pose notifications.
-        '''
+        collection for v1.0 firmware; this enables raw data but disables arm
+        and pose notifications.'''
 
         self.write_attr(0x28, b'\x01\x00')
         self.write_attr(0x1d, b'\x01\x00')
@@ -376,10 +350,9 @@ class MyoRaw(object):
         self.write_attr(0x19, b'\x01\x03\x01\x01\x00')
 
     def mc_end_collection(self):
-        '''Myo Connect sends this sequence (or a reordering) when ending data collection
-        for v1.0 firmware; this reenables arm and pose notifications, but
-        doesn't disable raw data.
-        '''
+        '''Myo Connect sends this sequence (or a reordering) when ending data
+        collection for v1.0 firmware; this reenables arm and pose
+        notifications, but doesn't disable raw data.'''
 
         self.write_attr(0x28, b'\x01\x00')
         self.write_attr(0x1d, b'\x01\x00')
@@ -396,9 +369,8 @@ class MyoRaw(object):
 
     def vibrate(self, length):
         if length in xrange(1, 4):
-            ## first byte tells it to vibrate; purpose of second byte is unknown
+            # first byte tells it to vibrate; purpose of second byte is unknown
             self.write_attr(0x19, pack('3B', 3, 1, length))
-
 
     def add_emg_handler(self, h):
         self.emg_handlers.append(h)
@@ -412,7 +384,6 @@ class MyoRaw(object):
     def add_arm_handler(self, h):
         self.arm_handlers.append(h)
 
-
     def on_emg(self, emg, moving):
         for h in self.emg_handlers:
             h(emg, moving)
@@ -425,77 +396,87 @@ class MyoRaw(object):
         for h in self.pose_handlers:
             h(p)
 
-    def on_arm(self, arm, xdir):
+    def on_arm(self, myoarm_msg):
         for h in self.arm_handlers:
-            h(arm, xdir)
+            h(myoarm_msg)
+
 
 if __name__ == '__main__':
-    # Start by initializing the Myo and attempting to connect. 
+    # Start by initializing the Myo and attempting to connect.
     # If no Myo is found, we attempt to reconnect every 0.5 seconds
-    connected = 0;
+    connected = 0
     print("Initializing...")
     while(connected == 0):
         try:
             m = MyoRaw(sys.argv[1] if len(sys.argv) >= 2 else None)
-            connected = 1;
+            connected = 1
         except (ValueError, KeyboardInterrupt) as e:
             print("Myo Armband not found. Attempting to connect...")
             rospy.sleep(0.5)
             pass
 
-    # Define Publishers
-    imuPub = rospy.Publisher('myo_imu', Imu, queue_size=10)
-    emgPub = rospy.Publisher('myo_emg', EmgArray, queue_size=10)
-    armPub = rospy.Publisher('myo_arm', MyoArm, queue_size=10, latch=True)
-    gestPub = rospy.Publisher('myo_gest', UInt8, queue_size=10)
+    rospy.init_node('myo_raw')
 
-    rospy.init_node('myo_raw', anonymous=True)
+    # Define Publishers
+    imuPub = rospy.Publisher('myo_imu', Imu, queue_size=1)
+    emgPub = rospy.Publisher('myo_emg', EmgArray, queue_size=1)
+    armPub = rospy.Publisher('myo_arm', MyoArm, queue_size=1, latch=True)
+    gestPub = rospy.Publisher('myo_gest', UInt8, queue_size=1)
+    gestStrPub = rospy.Publisher('myo_gest_str', String, queue_size=1)
 
     # Package the EMG data into an EmgArray
-    def proc_emg(emg, moving, times=[]):
-        ## create an array of ints for emg data
-        emgPub.publish(emg)
+    def proc_emg(emg, moving):
+        # create an array of ints for emg data
+        # and moving data
+        emgPub.publish(EmgArray(emg, moving))
 
-        ## print framerate of received data
-        times.append(time.time())
-        if len(times) > 20:
-            #print((len(times) - 1) / (times[-1] - times[0]))
-            times.pop(0)
     # Package the IMU data into an Imu message
     def proc_imu(quat1, acc, gyro):
-        # New info: https://github.com/thalmiclabs/myo-bluetooth/blob/master/myohw.h#L292-L295
+        # New info:
+        # https://github.com/thalmiclabs/myo-bluetooth/blob/master/myohw.h#L292-L295
         # Scale values for unpacking IMU data
-        # define MYOHW_ORIENTATION_SCALE   16384.0f ///< See myohw_imu_data_t::orientation
-        # define MYOHW_ACCELEROMETER_SCALE 2048.0f  ///< See myohw_imu_data_t::accelerometer
-        # define MYOHW_GYROSCOPE_SCALE     16.0f    ///< See myohw_imu_data_t::gyroscope
+        # define MYOHW_ORIENTATION_SCALE   16384.0f
+        # See myohw_imu_data_t::orientation
+        # define MYOHW_ACCELEROMETER_SCALE 2048.0f
+        # See myohw_imu_data_t::accelerometer
+        # define MYOHW_GYROSCOPE_SCALE     16.0f
+        # See myohw_imu_data_t::gyroscope
         if not any(quat1):
             # If it's all 0's means we got no data, don't do anything
             return
         h = Header()
         h.stamp = rospy.Time.now()
         h.frame_id = 'myo'
-        # We currently do not know the covariance of the sensors with each other
+        # We currently don't know the covariance of the sensors with each other
         cov = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        quat = Quaternion(quat1[0]/16384.0, quat1[1]/16384.0, quat1[2]/16384.0, quat1[3]/16384.0)
-        ## Normalize the quaternion and accelerometer values
-        quatNorm = math.sqrt(quat.x*quat.x+quat.y*quat.y+quat.z*quat.z+quat.w*quat.w)
-        normQuat = Quaternion(quat.x/quatNorm, quat.y/quatNorm, quat.z/quatNorm, quat.w/quatNorm)
-        normAcc = Vector3(acc[0]/2048.0, acc[1]/2048.0, acc[2]/2048.0)
-        normGyro = Vector3(gyro[0]/16.0, gyro[1]/16.0, gyro[2]/16.0)
+        quat = Quaternion(quat1[0] / 16384.0,
+                          quat1[1] / 16384.0,
+                          quat1[2] / 16384.0,
+                          quat1[3] / 16384.0)
+        # Normalize the quaternion and accelerometer values
+        quatNorm = math.sqrt(quat.x*quat.x + quat.y*quat.y + quat.z*quat.z + quat.w*quat.w)
+        normQuat = Quaternion(quat.x / quatNorm,
+                              quat.y / quatNorm,
+                              quat.z / quatNorm,
+                              quat.w / quatNorm)
+        normAcc = Vector3(acc[0] / 2048.0,
+                          acc[1] / 2048.0,
+                          acc[2] / 2048.0)
+        normGyro = Vector3(gyro[0] / 16.0, gyro[1] / 16.0, gyro[2] / 16.0)
         imu = Imu(h, normQuat, cov, normGyro, cov, normAcc, cov)
         imuPub.publish(imu)
 
-    # Package the arm and x-axis direction into an Arm message  
-    def proc_arm(arm, xdir):
-        #When the arm state changes, publish the new arm and orientation
-        #calibArm=MyoArm(arm.value, xdir.value)
-        calibArm=MyoArm(int(arm._keys[0]), int(xdir._keys[0]))
+    # Package the arm and x-axis direction into an Arm message
+    def proc_arm(myoarm_msg):
+        # When the arm state changes, publish the new arm and orientation
+        calibArm = myoarm_msg
         armPub.publish(calibArm)
 
     # Publish the value of an enumerated gesture
-    def proc_pose(p):
-        #gestPub.publish(p.value)
-        gestPub.publish(int(p._keys[0]))
+    def proc_pose(myo_pose):
+        gestPub.publish(myo_pose)
+        # myo_pose.
+        # gestStrPub.publish()
 
     m.add_emg_handler(proc_emg)
     m.add_imu_handler(proc_imu)
@@ -509,11 +490,9 @@ if __name__ == '__main__':
 
         while not rospy.is_shutdown():
             m.run(1)
-            #print("Run...")
 
     except (rospy.ROSInterruptException, serial.serialutil.SerialException) as e:
         print("Exception: " + str(e))
-        #pass
     finally:
         print()
         print("Disconnecting...")
